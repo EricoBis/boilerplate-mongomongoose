cd7070d8e6e35235d448dc31147815d8417759d4
Érico Bis <ericobis.santos@gmail.com>
2024-02-05 22:37:38 -0300

    add | person schema

--- myApp.js
+++ myApp.js
@@ -1,11 +1,20 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // MONGOOSE SETUP
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
 
-let Person;
+// let Person;
+const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
