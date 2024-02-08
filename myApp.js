4de8c75bb4a0329579c7558a56d6d508d136eb1a
Érico Bis <ericobis.santos@gmail.com>
2024-02-07 21:42:04 -0300

    add | removeById

--- myApp.js
+++ myApp.js
@@ -95,7 +95,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-
+  Person.findByIdAndRemove(personId, (err, removedPerson) => {
+    if (err) return console.log(err);
+    done(null, removedPerson);
+  });
 };
 
 const removeManyPeople = (done) => {
