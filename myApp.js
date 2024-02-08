c6604934610fd37a9094e13057baddd11110c620
Érico Bis <ericobis.santos@gmail.com>
2024-02-07 21:40:50 -0300

    add | findAndUpdate

--- myApp.js
+++ myApp.js
@@ -83,23 +83,30 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    (err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    },
+  );
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
 };
 
 /** **Well Done !!**
